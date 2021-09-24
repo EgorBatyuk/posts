e91a82f20a83528e3ea8128a2d797856f8f882a1
EgorBatyuk <fiching29@gmail.com>
2021-09-24 21:15:20 +0300

    Вывод из бд через json

--- polls/views.py
+++ polls/views.py
@@ -4,9 +4,10 @@ from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:10]
     output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    q = json.dumps(output)
+    return HttpResponse([q])
 
 
 def detail(request, question_id):
@@ -14,10 +15,11 @@ def detail(request, question_id):
 
 
 def results(request):
-    w = ['fwfw']
+    w = 1
+    t = 778
     q = json.dumps(w)
     print(q)
-    return HttpResponse(q)
+
 
 
 
